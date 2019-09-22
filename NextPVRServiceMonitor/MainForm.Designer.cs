2f513f6fd75e08d03d096ef9aa2fa037910e6c70
ChaosMageX <startrektommy@gmail.com>
2019-09-22 16:43:54 -0500

    Initial creation of app.

---
+++ NextPVRServiceMonitor/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace NextPVRServiceMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logTXT = new System.Windows.Forms.TextBox();
+            this.npvrLogLocLBL = new System.Windows.Forms.Label();
+            this.npvrLogLocTXT = new System.Windows.Forms.TextBox();
+            this.npvrLogLocBTN = new System.Windows.Forms.Button();
+            this.logLBL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // logTXT
+            // 
+            this.logTXT.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logTXT.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logTXT.Location = new System.Drawing.Point(13, 49);
+            this.logTXT.Multiline = true;
+            this.logTXT.Name = "logTXT";
+            this.logTXT.ReadOnly = true;
+            this.logTXT.Size = new System.Drawing.Size(559, 300);
+            this.logTXT.TabIndex = 0;
+            this.logTXT.WordWrap = false;
+            // 
+            // npvrLogLocLBL
+            // 
+            this.npvrLogLocLBL.AutoSize = true;
+            this.npvrLogLocLBL.Location = new System.Drawing.Point(13, 13);
+            this.npvrLogLocLBL.Name = "npvrLogLocLBL";
+            this.npvrLogLocLBL.Size = new System.Drawing.Size(126, 13);
+            this.npvrLogLocLBL.TabIndex = 1;
+            this.npvrLogLocLBL.Text = "NextPVR Log File Folder:";
+            // 
+            // npvrLogLocTXT
+            // 
+            this.npvrLogLocTXT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.npvrLogLocTXT.Location = new System.Drawing.Point(145, 10);
+            this.npvrLogLocTXT.Name = "npvrLogLocTXT";
+            this.npvrLogLocTXT.ReadOnly = true;
+            this.npvrLogLocTXT.Size = new System.Drawing.Size(346, 20);
+            this.npvrLogLocTXT.TabIndex = 2;
+            this.npvrLogLocTXT.Text = "C:\\Users\\Public\\NPVR\\Logs";
+            // 
+            // npvrLogLocBTN
+            // 
+            this.npvrLogLocBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.npvrLogLocBTN.Location = new System.Drawing.Point(497, 8);
+            this.npvrLogLocBTN.Name = "npvrLogLocBTN";
+            this.npvrLogLocBTN.Size = new System.Drawing.Size(75, 23);
+            this.npvrLogLocBTN.TabIndex = 3;
+            this.npvrLogLocBTN.Text = "Browse";
+            this.npvrLogLocBTN.UseVisualStyleBackColor = true;
+            this.npvrLogLocBTN.Click += new System.EventHandler(this.npvrLogLocBTN_Click);
+            // 
+            // logLBL
+            // 
+            this.logLBL.AutoSize = true;
+            this.logLBL.Location = new System.Drawing.Point(13, 33);
+            this.logLBL.Name = "logLBL";
+            this.logLBL.Size = new System.Drawing.Size(203, 13);
+            this.logLBL.TabIndex = 4;
+            this.logLBL.Text = "NextPVR Recording Service Activity Log:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.logLBL);
+            this.Controls.Add(this.npvrLogLocBTN);
+            this.Controls.Add(this.npvrLogLocTXT);
+            this.Controls.Add(this.npvrLogLocLBL);
+            this.Controls.Add(this.logTXT);
+            this.Name = "MainForm";
+            this.Text = "NextPVR Service Monitor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logTXT;
+        private System.Windows.Forms.Label npvrLogLocLBL;
+        private System.Windows.Forms.TextBox npvrLogLocTXT;
+        private System.Windows.Forms.Button npvrLogLocBTN;
+        private System.Windows.Forms.Label logLBL;
+    }
+}
+
---
+++ NextPVRServiceMonitor/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NextPVRServiceMonitor
+{
+    public partial class MainForm : Form
+    {
+        private const string cDefaultNpvrLogPath = "C:\\Users\\Public\\NPVR\\Logs";
+
+        private ServiceController mNPVRRecSC;
+
+        private StringBuilder mLogBuilder;
+        private StreamWriter mLogFileWriter;
+
+        // Should this be volatile?
+        private string mNpvrLogPath;
+
+        private bool bKeepRunning = true;
+        private Thread mMainThread;
+
+        private void initializeStuff()
+        {
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+            for (int i = scServices.Length - 1; i >= 0; i--)
+            {
+                mNPVRRecSC = scServices[i];
+                if (mNPVRRecSC.ServiceName == "NPVR Recording Service")
+                    break;
+            }
+
+            mLogBuilder = new StringBuilder();
+
+            string logFile = Path.Combine(Directory.GetCurrentDirectory(), "Log.txt");
+            mLogFileWriter = new StreamWriter(logFile, true);
+
+            mNpvrLogPath = Properties.Settings.Default.sLogFilePath;
+            if (!Directory.Exists(mNpvrLogPath))
+            {
+                mNpvrLogPath = cDefaultNpvrLogPath;
+                Directory.CreateDirectory(mNpvrLogPath);
+            }
+            npvrLogLocTXT.Text = mNpvrLogPath;
+        }
+
+        private void copyContents(string sourceDir, string targetDir)
+        {
+            Directory.CreateDirectory(targetDir);
+
+            string file;
+            string[] files = Directory.GetFiles(sourceDir, 
+                "*", SearchOption.TopDirectoryOnly);
+            for (int i = files.Length - 1; i >= 0; i--)
+            {
+                file = Path.Combine(targetDir, Path.GetFileName(files[i]));
+                File.Copy(files[i], file, true);
+            }
+
+            string dir;
+            string[] dirs = Directory.GetDirectories(sourceDir, 
+                "*", SearchOption.TopDirectoryOnly);
+            for (int j = dirs.Length - 1; j >= 0; j--)
+            {
+                dir = Path.Combine(targetDir, Path.GetFileName(dirs[j]));
+                copyContents(dirs[j], dir);
+            }
+        }
+
+        private void mainFunction()
+        {
+            DateTime nowDT;
+            string backupPath;
+            string logFolderName;
+            string logFolderParent;
+
+            while (bKeepRunning)
+            {
+                if (mNPVRRecSC.Status == ServiceControllerStatus.Stopped)
+                {
+                    nowDT = DateTime.Now;
+                    
+                    // Log and report the time the service stopped
+
+                    mLogBuilder.Append("Stopped ");
+                    mLogBuilder.AppendLine(nowDT.ToString("F"));
+                    logTXT.Text = mLogBuilder.ToString();
+
+                    mLogFileWriter.Write("Stopped ");
+                    mLogFileWriter.WriteLine(nowDT.ToString("F"));
+                    mLogFileWriter.Flush();
+
+                    // Backup the NextPVR logs
+
+                    logFolderName = Path.GetFileName(mNpvrLogPath);
+                    logFolderParent = Path.GetDirectoryName(mNpvrLogPath);
+
+                    backupPath = nowDT.ToString("_yyyy-dd-MM_HH\\hmm_ss");
+                    backupPath = string.Concat(logFolderName, backupPath);
+                    backupPath = Path.Combine(logFolderParent, backupPath);
+                    copyContents(mNpvrLogPath, backupPath);
+
+                    // Attempt to restart the service until successful
+
+                    mNPVRRecSC.Start();
+                    while (mNPVRRecSC.Status != ServiceControllerStatus.Running 
+                        && bKeepRunning)
+                    {
+                        Thread.Sleep(500);
+                        mNPVRRecSC.Refresh();
+                    }
+
+                    nowDT = DateTime.Now;
+
+                    // Log and report the time the service started again
+
+                    mLogBuilder.Append("Started ");
+                    mLogBuilder.AppendLine(nowDT.ToString("F"));
+                    logTXT.Text = mLogBuilder.ToString();
+
+                    mLogFileWriter.Write("Started ");
+                    mLogFileWriter.WriteLine(nowDT.ToString("F"));
+                    mLogFileWriter.Flush();
+                }
+                Thread.Sleep(1000);
+                mNPVRRecSC.Refresh();
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            initializeStuff();
+
+            mMainThread = new Thread(mainFunction);
+            mMainThread.Start();
+        }
+
+        private void npvrLogLocBTN_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.SelectedPath = mNpvrLogPath;
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                mNpvrLogPath = folderDialog.SelectedPath;
+                npvrLogLocTXT.Text = mNpvrLogPath;
+                Properties.Settings.Default.sLogFilePath = mNpvrLogPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            bKeepRunning = false;
+            Thread.Sleep(3000);
+            mLogFileWriter.Close();
+            Properties.Settings.Default.Save();
+        }
+    }
+}
